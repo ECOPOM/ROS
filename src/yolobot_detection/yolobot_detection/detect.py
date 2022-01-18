23fd43d1b50ff742f9e61d8fd5c20b62c6e0480f
Trim Bresilla <trim.bresilla@gmail.com>
2022-01-18 12:02:18 +0100

    ADD: added costum message

--- src/yolobot_detection/yolobot_detection/detect.py
+++ src/yolobot_detection/yolobot_detection/detect.py
@@ -30,6 +30,7 @@ class Detectron(Node):
 
         self.labeled_image = self.create_publisher(Image, 'image_labeled', 10)
         self.fps = self.create_publisher(Int8, 'fps_processing', 10)
+        self.detections = self.create_publisher(Int8, 'detections', 10)
         self.subscription = self.create_subscription(
             Image,
             'image_raw',
@@ -42,7 +43,6 @@ class Detectron(Node):
         frame = [frame]
         results = self.model(frame)
         labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
-        print(type(cords))
         return labels, cord
 
     def class_to_label(self, x):
@@ -62,21 +62,45 @@ class Detectron(Node):
 
         return frame
 
+    def pub_each(self, results, frame):
+        labels, cord = results
+        print(cord)
+ 
+        n = len(labels)
+        detections = Int8()
+        detections.data = n
+        self.detections.publish(detections)
+
+
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.3:
+                x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                bgr = (0, 255, 0)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+
+
+
+
     def callback(self, data):
         self.i += 1
         print("checking " + str(self.i))
         frame = bridge.imgmsg_to_cv2(data, "bgr8")
+
         start_time = time()
         results = self.score_frame(frame)
         frame = self.plot_boxes(results, frame)
         end_time = time()
-        fps = 1/np.round(end_time - start_time, 2)
+
+        self.pub_each(results, frame)
 
         labeled_frame = bridge.cv2_to_imgmsg(frame, encoding='bgr8')
         self.labeled_image.publish(labeled_frame)
 
         fps_data = Int8()
-        fps_data.data = int(fps)
+        fps_data.data = int(1/np.round(end_time - start_time, 2))
         self.fps.publish(fps_data)
 
 
