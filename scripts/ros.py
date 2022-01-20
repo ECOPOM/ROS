d23b6a5230da4a7c0ec80fc3bbecb03e83348780
Trim Bresilla <trim.bresilla@gmail.com>
2022-01-21 00:35:51 +0100

    added a rosbridge example

---
+++ scripts/ros.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import roslibpy
+import json
+
+client = roslibpy.Ros(host='10.60.64.2', port=9090)
+client.run()
+
+
+data={}
+def receive_message(msg):
+    data['detections'] = msg['data']
+    # print('Heard talking: ' + str(msg['data']))
+
+listener = roslibpy.Topic(client, '/yolobot/detections', 'std_msgs/Int8')
+listener.subscribe(receive_message)
+
+try:
+    while True:
+        print(json.dumps(data, indent=2))
+except KeyboardInterrupt:
+    client.terminate()
